4ad7a799af18869977ca5c64f4b8a8be7f48870f
bwilson <bwilson@bridge2solutions.com>
2017-08-06 19:26:49 -0400

    added material design

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import {styles} from './scss/styles.scss';
 
-function component() {
-	var element = document.createElement('div');
-	element.className = "alert alert-success";
-	element.setAttribute("role", "alert");
-	element.innerHTML = "Webpack Scaffolding is running using Bootstrap 4";
+function MaterialComponent() {
+	var element = document.createElement('button');
+
+
+	element.className = "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect";
+	element.innerHTML = "Matarial Design Button";
 
 	return element;
 }
 
-document.body.appendChild(component());
+function BootstrapComponent() {
+	var element = document.createElement('div');
+
+
+	element.className = "alert alert-success";
+	element.setAttribute("role", "alert");
+	element.innerHTML = "Bootstrap 4 Alert";
+
+	return element;
+}
+
+document.body.appendChild(MaterialComponent());
+document.body.appendChild(BootstrapComponent());
